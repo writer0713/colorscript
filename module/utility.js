a2fe97caf399d6c2496a7c227372efaea8763521
writer0713 <writer0713@naver.com>
2015-09-24 10:10:13 +0800

    크롬, 파폭에서 제대로 동작하게 대응

--- module/utility.js
+++ module/utility.js
@@ -22,13 +22,21 @@ _log('utility.js is loaded successfully');
       var ol = $('div#linenumber ol');
       /* div가 한개 남았을때 백스페이스로 지울경우 div가 지워지면서 갯수가 0이 된다.
       이때 div를 하나 추가해주고 div_count를 강제로 1로 변환해준다.*/
-      var div_count_in_pre = ($('pre#code-text div').length == 0) ? (function() {
-        $('pre#code-text').find('br').remove().end().append('<div></div>');
-        return 1;
-      })() : $('pre#code-text div').length;
+      var p_count_in_pre = ($('pre#code-text p').length == 0) ? (function() {
+        $('pre#code-text').find('br').remove().end().append("<p id='first_p'></p>"); // br을 p로 바꿔준다
+        // 커서를 새로 만들어준 p tag에 포커싱 시켜줘야 그 다음에 엔터를 치면 p 태그로 생성됨(안그러면 br 로 생성)
+        var el = document.getElementById('first_p');
+            range = document.createRange();
+            range.selectNodeContents(el);
+            var sel = window.getSelection();
+            sel.removeAllRanges();
+            sel.addRange(range);
+
+        return 1; // p tag의 갯수가 1개라고 강제로 지정
+      })() : $('pre#code-text p').length;
 
       var linenumber = $('div#linenumber ol li').length;
-      var len = div_count_in_pre - linenumber;
+      var len = p_count_in_pre - linenumber;
       var min_linenum_to_add = linenumber + 1;
       var i = 0;
 
